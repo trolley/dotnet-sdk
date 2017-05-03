be139105acd608416397157c468327760be46c69
butt- <butt-@LAPTOP-6IDPB8FM>
2017-05-03 17:53:59 -0400

    Built part of PayoutHelper Built all structs required to parse JSON Payout to C# object

--- paymentrails/Types/Payout.cs
+++ paymentrails/Types/Payout.cs
@@ -8,5 +8,129 @@ namespace paymentrails.Types
 {
     public struct Payout
     {
+        private double autoswitchLimit;
+        private bool autoswitchActive;
+        private double holdupLimit;
+        private bool holdupActive;
+        private string primaryMethod;
+        private string primaryCurrency;
+        private BankAccount bank;
+        private PaypalAccount paypal;
+
+        public double AutoswitchLimit
+        {
+            get
+            {
+                return autoswitchLimit;
+            }
+
+            set
+            {
+                autoswitchLimit = value;
+            }
+        }
+
+        public bool AutoswitchActive
+        {
+            get
+            {
+                return autoswitchActive;
+            }
+
+            set
+            {
+                autoswitchActive = value;
+            }
+        }
+
+        public double HoldupLimit
+        {
+            get
+            {
+                return holdupLimit;
+            }
+
+            set
+            {
+                holdupLimit = value;
+            }
+        }
+
+        public bool HoldupActive
+        {
+            get
+            {
+                return holdupActive;
+            }
+
+            set
+            {
+                holdupActive = value;
+            }
+        }
+
+        public string PrimaryMethod
+        {
+            get
+            {
+                return primaryMethod;
+            }
+
+            set
+            {
+                primaryMethod = value;
+            }
+        }
+
+        public string PrimaryCurrency
+        {
+            get
+            {
+                return primaryCurrency;
+            }
+
+            set
+            {
+                primaryCurrency = value;
+            }
+        }
+
+        public BankAccount Bank
+        {
+            get
+            {
+                return bank;
+            }
+
+            set
+            {
+                bank = value;
+            }
+        }
+
+        public PaypalAccount Paypal
+        {
+            get
+            {
+                return paypal;
+            }
+
+            set
+            {
+                paypal = value;
+            }
+        }
+
+        public Payout(double autoswitchLimit, bool autoswitchActive, double holdupLimit, bool holdupActive, string primaryMethod, string primaryCurrency, BankAccount bank, PaypalAccount paypal)
+        {
+            this.autoswitchLimit = autoswitchLimit;
+            this.autoswitchActive = autoswitchActive;
+            this.holdupLimit = holdupLimit;
+            this.holdupActive = holdupActive;
+            this.primaryMethod = primaryMethod;
+            this.primaryCurrency = primaryCurrency;
+            this.bank = bank;
+            this.paypal = paypal;
+        }
     }
 }
