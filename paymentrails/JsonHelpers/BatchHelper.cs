a0296a8f0bc543030130e9579996d4df6b8162dd
Jesse Silber <jessesilber@gmail.com>
2017-05-08 16:11:10 -0400

    Payment/Batches, query/get methods return lists

--- paymentrails/JsonHelpers/BatchHelper.cs
+++ paymentrails/JsonHelpers/BatchHelper.cs
@@ -10,6 +10,21 @@ namespace paymentrails.JsonHelpers
 {
     class BatchHelper : JsonHelper
     {
+
+        public static List<Batch> JsonToBatchList(string jsonResponse)
+        {
+            BatchListJsonHelper helper = new JavaScriptSerializer().Deserialize<BatchListJsonHelper>(jsonResponse);
+            List<Batch> batches = new List<Batch>();
+            if (helper.Ok)
+            {
+                foreach (BatchJsonHelper b in helper.Batches)
+                {
+                    batches.Add(BatchJsonHelperToBatch(b));
+                }
+            }
+            return batches;
+        }
+
         public static Batch JsonToBatch(string JsonResponse)
         {
             BatchResponseJsonHelper helper = new JavaScriptSerializer().Deserialize<BatchResponseJsonHelper>(JsonResponse);
--- paymentrails/Program.cs
+++ paymentrails/Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Net;
 using System.Collections.Generic;
 using System.Web;
+using paymentrails.Types;
+
 namespace paymentrails
 {
     //REMOVE THIS ONCE COMPLETED SINCE THIS PROJECT IS NOT A CONSOLE APP IT IS A LIBRARY
@@ -20,24 +22,34 @@ namespace paymentrails
 
             // local
             PaymentRails_Configuration.setApiBase("http://api.local.dev:3000");
-            PaymentRails_Configuration.apiKey = "pk_test_91XPUY8D8GAGA";
+            PaymentRails_Configuration.apiKey = "pk_test_91XPYV1Y8MXQC";
 
-            Types.Recipient recipient = PaymentRails_Recipient.get("R-91XQ00KM0CPMR");
-            recipient.Payout.PrimaryMethod = "paypal";
-            recipient.Email = "test@test.test";
-            recipient.Payout.AutoswitchActive = true;
-            Console.WriteLine(recipient);
-            PaymentRails_Recipient.patch(recipient);
-            recipient = PaymentRails_Recipient.get("R-91XQ00KM0CPMR");
-            //string val = PaymentRails_Payment.get("P-908GY52558A7R");
-            //var payment = JsonHelpers.PaymentHelper.JsonToPayment(val);
+            //Types.Recipient recipient = PaymentRails_Recipient.get("R-91XQ00KM0CPMR");
+            //recipient.Payout.PrimaryMethod = "paypal";
+            //recipient.Email = "test@test.test";
+            //recipient.Payout.AutoswitchActive = true;
+            //Console.WriteLine(recipient);
+            //PaymentRails_Recipient.patch(recipient);
+            //recipient = PaymentRails_Recipient.get("R-91XQ00KM0CPMR");
 
-            //string val = PaymentRails_Batch.get("B-912Q4PAPBN868");
-            //Types.Batch batch = JsonHelpers.BatchHelper.JsonToBatch(val);
+            List<Payment> payments = PaymentRails_Payment.get(1,30);
 
-            //Console.WriteLine(batch);
+            List<Batch> batches = PaymentRails_Batch.get(1,30);
 
-            Console.Read();
+            Batch batch = PaymentRails_Batch.get("B-91XQ40VXECQJM");
+
+            List<Recipient> recipients = PaymentRails_Recipient.get(1,30);
+            List<Recipient> moreRecips = PaymentRails_Recipient.query();
+
+                //string val = PaymentRails_Payment.get("P-908GY52558A7R");
+                //var payment = JsonHelpers.PaymentHelper.JsonToPayment(val);
+
+                //string val = PaymentRails_Batch.get("B-912Q4PAPBN868");
+                //Types.Batch batch = JsonHelpers.BatchHelper.JsonToBatch(val);
+
+                //Console.WriteLine(batch);
+
+                Console.Read();
 
         }       
     }
