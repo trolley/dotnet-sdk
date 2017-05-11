67c939e62846c2d70081588ebedf708b0bad348a
butt- <jessetrem@gmail.com>
2017-05-11 17:23:42 -0400

    Added JsonToPayout unit tests
    Added a test for a payout with a paypal
    
    Updated test for payout with bank account so the bank account has more
    fields

--- paymentrailsTest/JsonHelper/PayoutHelperTest.cs
+++ paymentrailsTest/JsonHelper/PayoutHelperTest.cs
@@ -20,12 +20,22 @@ namespace paymentrailsTest.JsonHelper
         [TestMethod]
         public void testJsonToPayoutWithBankAccount()
         {
-            BankAccount bank = new BankAccount("BR", null, null, null, "USD", "Yupg", null, "*************************93P1", null, "EMPTY", new Address(null, null, null, null, null, "BR", null));
+            BankAccount bank = new BankAccount("BR", null, null, null, "USD", "Yupg", null, "*************************93P1", null, "EMPTY", new Address("123 StreetPlace", null, "Sao Paulo", null, null, "BR", "Brazilia"));
             Payout payout = new Payout(1000, false, 1000, false, "bank", "USD", bank, null);
-            String response = @"{""ok"":true,""autoswitch"":{""limit"":1000,""active"":false},""holdup"":{""limit"":1000,""active"":false},""primary"":{""method"":""bank"",""currency"":{""currency"":{""code"":""USD"",""name"":""US Dollar""}}},""method"":""bank"",""accounts"":{""bank"":{""currency"":""USD"",""country"":""BR"",""name"":""Yupg"",""iban"":""*************************93P1"",""governmentId"":""EMPTY""}}}";
+            String response = @"{""ok"":true,""autoswitch"":{""limit"":1000,""active"":false},""holdup"":{""limit"":1000,""active"":false},""primary"":{""method"":""bank"",""currency"":{""currency"":{""code"":""USD"",""name"":""US Dollar""}}},""method"":""bank"",""accounts"":{""bank"":{""currency"":""USD"",""country"":""BR"",""name"":""Yupg"",""iban"":""*************************93P1"",""bankRegion"":""Brazilia"",""bankCity"":""Sao Paulo"",""bankAddress"":""123 StreetPlace"",""governmentId"":""EMPTY""}}}";
             Payout newPayout = PaymentRails.JsonHelpers.PayoutHelper.JsonToPayout(response);
 
             Assert.AreEqual(payout, newPayout);
+        }
+
+        [TestMethod]
+        public void testJsonToPayoutWithPaypalAccount()
+        {
+            PaypalAccount paypal = new PaypalAccount("sample32@email.com");
+            Payout payout = new Payout(1000, false, 1000, false, "paypal", "USD", null, paypal);
+            string response = @"{""ok"":true,""autoswitch"":{""limit"":1000,""active"":false},""holdup"":{""limit"":1000,""active"":false},""primary"":{""method"":""paypal"",""currency"":{""currency"":{""code"":""USD"",""name"":""US Dollar""}}},""method"":""paypal"",""accounts"":{""paypal"":{""address"":""sample32@email.com""}}}";
+            Payout responsePayout = PaymentRails.JsonHelpers.PayoutHelper.JsonToPayout(response);
+            Assert.AreEqual(payout, responsePayout);
         }
 
         [TestMethod]
