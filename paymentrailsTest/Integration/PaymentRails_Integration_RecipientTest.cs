bd1f3c4d2ac9ff4dc6f61fbcca50e60c30b9fa84
Jesse Tremblay <jessetrem@gmail.com>
2018-08-23 14:57:29 -0400

    fixed issue where isPrimary was not being sent to api

--- paymentrailsTest/Integration/PaymentRails_Integration_RecipientTest.cs
+++ paymentrailsTest/Integration/PaymentRails_Integration_RecipientTest.cs
@@ -81,11 +81,11 @@ namespace paymentrailsTest
             Assert.IsNotNull(recipient.email.IndexOf(uuid));
             Assert.IsNotNull(recipient.id);
 
-            RecipientAccount recipientAccount = new RecipientAccount("bank-transfer", "EUR", null, null, "FR", "FR14 2004 1010 0505 0001 3M02 606", "123456");
+            RecipientAccount recipientAccount = new RecipientAccount("bank-transfer", "EUR", null, "true", "FR", "FR14 2004 1010 0505 0001 3M02 606", "123456");
             recipientAccount = gateway.recipientAccount.create(recipient.id, recipientAccount);
             Assert.IsNotNull(recipientAccount);
 
-            recipientAccount = new RecipientAccount("bank-transfer", "EUR", null, null, "DE", "DE89 3704 0044 0532 0130 00", "123456");
+            recipientAccount = new RecipientAccount("bank-transfer", "EUR", null, "true", "DE", "DE89 3704 0044 0532 0130 00", "123456");
             recipientAccount = gateway.recipientAccount.create(recipient.id, recipientAccount);
             Assert.IsNotNull(recipientAccount);
 
