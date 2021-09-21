f805e25eac625bfe6b5f18cfbd444da836005c4a
Joshua Cunningham <jbc.okay@gmail.com>
2021-09-21 12:08:09 -0400

    remove credentials

--- tests/config.cs
+++ tests/config.cs
@@ -2,8 +2,8 @@
 {
     static class Config
     {
-        public const string TEST_API_KEY = "ASXQRXUnW02MCRVZ8ZBVJGFH";
-        public const string TEST_API_SECRET = "4m5t5xap00dy9cyetthfxwy6vunef55ern0bed3r";
-        public const string TEST_BALANCE_CURRENCY = "GBP";
+        public const string TEST_API_KEY = "";
+        public const string TEST_API_SECRET = "";
+        public const string TEST_BALANCE_CURRENCY = "";
     }
 }
